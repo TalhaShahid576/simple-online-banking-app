216b687c945920ac275cfbb945c0786fe8451b8e
Talha Shahid <m.talhas1993@gmail.com>
2023-02-07 19:50:19 +0500

    Dashboard updates

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Home = ({ user, getUser }) => {
   React.useEffect(() => {
-    getUser();
+    // getUser();
   }, [getUser]);
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Dashboard</h1>
       {user && (
         <>
           <p>Welcome, {user.name}</p>
